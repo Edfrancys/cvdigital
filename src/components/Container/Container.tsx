fababaf5a770d64e0a699616fd79cda44134d335
root <root@Edfrancys.edfrancys.io>
2022-02-16 10:36:21 -0300

    component container creating

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {motion} from 'framer-motion';
+
+import styled from './Container.module.css'
+
+export const Container: React.FC = () => {
+    return (
+        <motion.section
+            className={styled.}
+        >
+
+        </motion.section>
+    );
+};
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import React from 'react';
+import { Container } from '../../components/Container';
+
+const Home: NextPage = () => {
+    return (
+        <Container>
+          Hello Word!
+        </Container>
+    );
+};
+
+export default Home;
